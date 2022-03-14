6fd94f9b7da92b1ab3f262ec1878b7c19191c54f
Andreas Asatera <tolong@buatin.website>
2022-03-14 13:35:08 +0700

    Fix Model

--- app/Models/PokeModel.php
+++ app/Models/PokeModel.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class PokeModel extends Model
 {
     protected $table = 'siswa_tbl';
+    protected $fillable = [
+        'nis',
+        'nama',
+        'hobi',
+        'umur',
+        'alamat',
+    ];
+    public $timestamps = false;
 }
 
